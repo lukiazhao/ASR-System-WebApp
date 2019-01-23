e386d6857fcbab04d13b0c68b8c993933cb39274
rmit-s3681602-JingYi-Zhao <s3681602@student.rmit.edu.au>
2019-01-24 00:53:30 +1100

    Initial

---
+++ AsrSystem/AsrSystem/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsrSystem.Models
+{
+    public class Student
+    {
+
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string StudentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
